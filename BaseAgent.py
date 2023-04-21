5d0da5a1f0a386bbad724b9d30e957373fda8c30
Ivo Schols <IvoSchols97@gmail.com>
2023-04-21 11:44:46 +0200

    Refactor baseagent

--- BaseAgent.py
+++ BaseAgent.py
@@ -2,9 +2,11 @@ from abc import ABC, abstractmethod
 import torch.nn as nn
 import torch 
 import numpy as np
+import torch.optim as optim
+
 
 class Agent(ABC):
-    def __init__(self, env, device):
+    def __init__(self, env, device, optimizer='adam', lossFunction='huber'):
         self.env = env
         self.device = device
         self.learning_rate = 1e-4
@@ -14,6 +16,28 @@ class Agent(ABC):
         self.beta = 0.3        # Determines chance of curiosity\
 
         self.policy = 'egreedy'
+
+        match optimizer:
+            case 'adam':
+                self.optimizer = optim.Adam(self.net.parameters(), lr=self.learning_rate)
+            case 'rmsprop':
+                self.optimizer = optim.RMSprop(self.net.parameters(), lr=self.learning_rate)
+            case 'sgd':
+                self.optimizer = optim.SGD(self.net.parameters(), lr=self.learning_rate)
+            case other:
+                self.optimizer = optim.Adam(self.net.parameters(), lr=self.learning_rate)
+        
+        match lossFunction:
+            case 'huber':
+                self.lossFunction = nn.SmoothL1Loss()
+            case 'mse':
+                self.lossFunction = nn.MSELoss()
+            case 'crossentropy':
+                self.lossFunction = nn.CrossEntropyLoss()
+            case other:
+                self.lossFunction = nn.SmoothL1Loss()
+   
+
 
 
 
--- ReinforceAgent.py
+++ ReinforceAgent.py
@@ -5,15 +5,13 @@ import numpy as np
 import torch.nn as nn
 from BaseAgent import Agent
 from ReinforceNetwork import ReinforceModel
-import torch.optim as optim
 
 class ReinforceAgent(Agent):
 
-    def __init__(self, env, device, optimiser='adam', lossFunction='huber'):
-        super().__init__(env, device, optimiser, lossFunction)
-        self.loss_fn = nn.CrossEntropyLoss()
+    def __init__(self, env, device, optimizer='adam', lossFunction='huber'):
+        super().__init__(env, device, optimizer, lossFunction)
         self.policy = ReinforceModel()
-        self.optimizer = optim.Adam(self.policy.parameters(), lr=0.01)
+        
 
     def optimize_model(self, state, action, next_state, reward, done, rewards):
         log_probs = torch.stack(log_probs)
